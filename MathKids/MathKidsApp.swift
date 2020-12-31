8178f01740ae1080634d53e789eaaca197d4c763
Waleerat S. Gottlieb <waleerat.gottlieb@icloud.com>
2020-12-31 08:33:16 +0100

    Initial Commit

---
+++ MathKids/MathKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathKidsApp.swift
+//  MathKids
+//
+//  Created by Waleerat Gottlieb on 2020-12-31.
+//
+
+import SwiftUI
+
+@main
+struct MathKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
